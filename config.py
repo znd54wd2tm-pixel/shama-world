05f1390c08500497fc64fff76e6cc220d9c0fb02
znd54wd2tm-pixel <znd54wd2tm@privaterelay.appleid.com>
2026-09-23 20:42:28 +0300

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 import os
-BOT_TOKEN=os.getenv("BOT_TOKEN")
-if not BOT_TOKEN: raise RuntimeError("BOT_TOKEN environment variable is not set")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN environment variable is not set")
